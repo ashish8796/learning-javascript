b13cc58fa03d49be11a172c7a0ae8c7177e1da8f
ashish8796 <ashishsaini.ak@gmail.com>
2020-03-31 00:16:53 +0530

    primer added

--- day-2/getting-into-javascript/exercises/primer/ex.js
+++ day-2/getting-into-javascript/exercises/primer/ex.js
@@ -1,13 +1,19 @@
 // TODO: define addFavoriteBook(..) function
+
 function addFavoriteBook(bookName) {
-  if (!bookName.includes("Great")){
-    favoriteBooks[bookName];
+  if (!bookName.includes("Great")) {
+    favoriteBooks.push(bookName);
   }
 }
 // TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+  console.log(`Favorite Books: ${favoriteBooks.length}`);
+  for (let book of favoriteBooks) {
+    console.log(book);
+  }
+}
 
 var favoriteBooks = [];
-console.log(favoriteBooks);
 
 addFavoriteBook("A Song of Ice and Fire");
 addFavoriteBook("The Great Gatsby");
@@ -16,3 +22,4 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
+printFavoriteBooks();
