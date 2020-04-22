c4fe6617870b0a48375202df62498abcc96f169c
ashish8796 <ashishsaini.ak@gmail.com>
2020-04-22 23:09:27 +0530

    6kyu problem kata added

---
+++ codewarkatas/binaries/index.js
@@ -0,0 +1,44 @@
+// Task:
+// Given strng a string of digits representing a decimal number the function code(strng) should return the coding of strng as explained above.
+// Given a string strng resulting from the previous coding, decode it to get the corresponding decimal string.
+
+let map = new Map();
+
+for (let i = 0; i <= 9; i++) {
+  let n = Number(i).toString(2);
+  let k = n.length;
+  let val = "";
+  for (let i = 0; i < k-1; i++) {
+    val += "0"
+  }
+  val += "1" + n;
+
+  map.set(i + '', val)
+}
+
+function code(strng) {
+  resStr = "";
+  strng.split('').forEach(element => {
+    resStr += map.get(element)
+  });
+  return resStr;
+}
+
+function decode(result) {
+  let ans = "";
+
+  while (result.length != 0) {
+    for (let key of map.keys()) {
+      if (RegExp("^" + map.get(key)).test(result)) {
+        ans += key;
+        result = result.replace(map.get(key), "")
+      }
+    }
+  }
+  return ans;
+}
+
+result = "001101001101011101110011110011111010";
+code("55337700")
+decode(result);
+console.log(code("55337700")===decode(result))
