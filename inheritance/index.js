07955b40102725d4e4360f2dc0e2d1568a8dd74e
ashish8796 <ashishsaini.ak@gmail.com>
2020-05-25 21:36:25 +0530

    inhertance updated

--- inheritance/index.js
+++ inheritance/index.js
@@ -1,3 +1,14 @@
-function Animals() {
-  
+function Animals(name, legs) {
+  this.name = name;
+  this.legs = legs;  
 }
+
+let animal1 = new Animals("rabbit", 4)
+animal1.run = "fast";
+animal1.name = "dog";
+
+let animal2 = new Animals ("cow", 4);
+animal2.givesMilk = true;
+
+console.log(animal1)
+console.log(animal2)
